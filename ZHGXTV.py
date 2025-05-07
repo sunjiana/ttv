0a1a649820d34e0759c6338edfd2ded655aaf628
sunjiana <61388089+sunjiana@users.noreply.github.com>
2025-05-07 20:53:15 +0800

    Update ZHGXTV.py

--- ZHGXTV.py
+++ ZHGXTV.py
@@ -711,7 +711,7 @@ results.sort(key=lambda x: channel_key(x[0]))
 
 result_counter = 8  # 每个频道需要的个数
 
-with open("itvlist.txt", 'w', encoding='utf-8') as file:
+with open("ZHGXTV.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('央视频道,#genre#\n')
     for result in results:
@@ -755,7 +755,7 @@ with open("itvlist.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
 
-with open("itvlist.m3u", 'w', encoding='utf-8') as file:
+with open("ZHGXTV.m3u", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('#EXTM3U\n')
     for result in results:
